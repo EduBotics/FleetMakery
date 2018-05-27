8259cb85e208539fc1f4dad34d54c1e62f28fc0d
Michael <32701347+EduBotics@users.noreply.github.com>
2018-05-27 11:03:37 +0100

    initial test of parameterised traffic lights

--- Pibrella/traffic_lights.py
+++ Pibrella/traffic_lights.py
@@ -1,18 +1,29 @@
 import pibrella
 from time import sleep
 
+SEQUENCE = [RED, RED_AMBER, GREEN, AMBER] = range(4)
+
+TIMING = {
+    RED: 3,
+    RED_AMBER: 4,
+    GREEN: 6,
+    AMBER: 4
+}
+
+ACTIONS = {
+    RED: pibrella.light.red.on,
+    RED_AMBER: lambda: pibrella.light.off() && pibrella.light.red.on() && pibrella.light.amber.on(),
+    GREEN: pibrella.light.green.on,
+    AMBER: pibrella.light.amber.blink(0.5,0.5)
+}
+
 def traffic_lights():
     pibrella.light.off()
-    pibrella.light.red.on()
-    sleep(3)
-    pibrella.light.amber.on()
-    sleep(4)
-    pibrella.light.off()
-    pibrella.light.green.on()
-    sleep(6)
-    pibrella.light.off()
-    pibrella.light.amber.blink(0.5,0.5)
-    sleep(4)
-    pibrella.light.off()
+    for step in SEQUENCE:
+        action = ACTIONS[step]
+        dur = TIMINGS[step]
+        action()
+        sleep(dur)
+
 
 traffic_lights()
