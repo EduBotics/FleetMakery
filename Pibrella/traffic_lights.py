eb6a222f8d13feab7e1e916b6fe225193f8c2089
Michael <32701347+EduBotics@users.noreply.github.com>
2018-05-27 10:44:39 +0100

    First Traffic Lights code

---
+++ Pibrella/traffic_lights.py
@@ -0,0 +1,15 @@
+def traffic_lights():
+    pibrella.light.off()
+    pibrella.light.red.on()
+    sleep(3)
+    pibrella.light.amber.on()
+    sleep(4)
+    pibrella.light.off()
+    pibrella.light.green.on()
+    sleep(6)
+    pibrella.light.off()
+    pibrella.light.amber.blink(0.5,0.5)
+    sleep(4)
+    pibrella.light.off()
+
+traffic_lights()
