7aefb93835d5a40802dc65c31acf529c8631a5b8
Michael Noel Jones <mnjones+git@gmail.com>
2018-05-28 14:56:25 +0100

    Add TK HelloWorld code

--- Python/HelloWorld/helloworld.py
+++ Python/HelloWorld/helloworld.py
@@ -1,1 +1,11 @@
+<<<<<<< Updated upstream
 print("Hello World!")
+=======
+from turtle import TK
+root = TK.Tk()
+canvas = TK.Canvas(root, width=500, height=500, bg="#ddffff")
+canvas.pack()
+canvas.create_text(200, 250, fill="darkblue", font="Times 20 italic bold", text="Hello World!")
+
+TK.mainloop()
+>>>>>>> Stashed changes
