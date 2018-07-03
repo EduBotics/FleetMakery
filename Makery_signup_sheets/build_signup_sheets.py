c71440a1c3b807d7a83e866b6f2c8f8f471e19f7
Michael <32701347+EduBotics@users.noreply.github.com>
2018-07-03 13:48:50 +0100

    Changes to bring in working(\!) hierarchical event structure

--- Makery_signup_sheets/build_signup_sheets.py
+++ Makery_signup_sheets/build_signup_sheets.py
@@ -1,141 +1,74 @@
 # -*- coding: utf-8 -*-
 """Front end to generate signup sheets from data"""
 
-from gen_signup_sheet import SignupSheet
-
-
-class Event(object):
-    def __init__(self, params=None):
-        self._cost = 8
-        self._eventname = None
-        self._date = None
-        self._dow = None
-        self._starttime = None
-        self._endtime = None
-        self._cost = None
-        self._plu = None
-        self._description = None
-        self._agerange = None
-        self._numslots = None
-        if params is not None:
-            self.override(params)
-
-    @property
-    def params(self):
-        params = {}
-        for key in [
-            "cost", "eventname", "date", "dow", "starttime", "endtime", "cost",
-            "plu", "description", "agerange", "numslots"
-        ]:
-            params[key] = getattr(self, "_" + key, None)
-        return params
-
-    @property
-    def eventname(self):
-        return self._eventname
-
-    @eventname.setter
-    def eventname(self, value):
-        self._eventname = value
-
-    @property
-    def date(self):
-        return self._date
-
-    @date.setter
-    def date(self, value):
-        self._date = value
-
-    @property
-    def dow(self):
-        return self._dow
-
-    @dow.setter
-    def dow(self, value):
-        self._dow = value
-
-    @property
-    def starttime(self):
-        return self._starttime
-
-    @starttime.setter
-    def starttime(self, value):
-        self._starttime = value
-
-    @property
-    def endtime(self):
-        return self._endtime
-
-    @endtime.setter
-    def endtime(self, value):
-        self._endtime = value
-
-    @property
-    def cost(self):
-        return self._cost
-
-    @cost.setter
-    def cost(self, value):
-        self._cost = value
-
-    @property
-    def plu(self):
-        return self._plu
-
-    @plu.setter
-    def plu(self, value):
-        self._plu = value
-
-    @property
-    def description(self):
-        return self._description
-
-    @description.setter
-    def description(self, value):
-        self._description = value
-
-    @property
-    def agerange(self):
-        return self._agerange
-
-    @agerange.setter
-    def agerange(self, value):
-        self._agerange = value
-
-    @property
-    def numslots(self):
-        return self._numslots
-
-    @numslots.setter
-    def numslots(self, value):
-        self._numslots = value
-
-    def override(self, params):
-        for key in params:
-            setattr(self, "_" + key, params[key])
-
+from gen_signup_sheet import SignupSheet, Event
 
 event_types = {
-    "base": Event({
-        "cost": 8
-    }),
+    "base": Event({"cost": 8})
 }
 
 event_types_overrides = {
     "saturday": [
-        "base", {
-            "starttime": "11:00",
-            "endtime": "13:00",
+        "base",
+        {
+            "dow": "Sat",
+            "starttime": "12:00",
+            "endtime": "14:00",
             "agerange": "8-13",
             "plu": "2x PLU8"
         }
     ],
     "afterschool": [
-        "base", {
+        "base",
+        {
             "starttime": "15:45",
             "endtime": "17:45",
             "agerange": "8-13",
             "plu": "2x PLU8"
+        }
+    ],
+    "adult": [
+        "base",
+        {
+            "dow": "Tue",
+            "starttime": "11:00",
+            "endtime": "13:00",
+            "agerange": "17+",
+            "plu": ""
+        }
+    ],
+    "summer_tuesday": [
+        "base",
+        {
+            "starttime": "",
+            "endtime": "",
+            "agerange": "8-13",
+            "plu": "PLU7",
+        }
+    ],
+    "summer_weekday": [
+        "base",
+        {
+            "starttime": "15:00",
+            "endtime": "17:00",
+            "agerange": "8-13",
+            "plu": "PLU7",
+        }
+    ],
+    "summer_weekend": [
+        "saturday",
+        {
+            "plu": "PLU7",
+        }
+    ],
+    "scratch": [
+        "base",
+        {
+            "eventname": "Raspberry Pi Programming (Scratch)",
+            "description": (
+                "A chance to learn or improve your Scratch programming skills!"
+            ),
+            "numslots": 7,
         }
     ],
     "python": [
@@ -143,8 +76,7 @@ event_types_overrides = {
         {
             "eventname": "Raspberry Pi Programming (Python)",
             "description": (
-                "Use our Lego EV3 Mindstorms robots to "
-                "complete a selection of challenges."
+                "A chance to learn or improve your Python programming skills!"
             ),
             "numslots": 7,
         }
@@ -175,55 +107,72 @@ event_types_overrides = {
     ]
 }
 
+retry = []
+for name, override in event_types_overrides.iteritems():
+    print "Overriding {} with {} -> {}\n".format(override[0], override[1], name)
+    try:
+        event_types[name] = Event(orig=event_types[override[0]], params=override[1])
+    except KeyError:
+        retry.append(name)
+
+while len(retry):
+    name = retry.pop(0)
+    override = event_types_overrides[name]
+    print "Overriding {} with {} -> {}\n".format(override[0], override[1], name)
+    try:
+        event_types[name] = Event(orig=event_types[override[0]], params=override[1])
+    except AttributeError as err:
+        print "\t{} : Error: {}\n".format(name, err)
+        if len(retry) > 1:
+            retry.append(name)
 
 the_events = [
     {
-        "eventname": "Lego Robotics",
-        "date": "23/7/2018",
-        "dow": "Mon",
-        "starttime": "15:00",
-        "endtime": "17:00",
-        "cost": "8",
-        "plu": "",
-        "description": (
-            "Use our Lego EV3 Mindstorms robots to "
-            "complete a selection of challenges."
-        ),
-        "agerange": "8-13",
-        "numslots": 4,
+        "name": "20180723_robotics_summer", 
+        "bases": ["summer_weekday", "robotics"],
+        "overrides": {
+            "eventname": "Summer Session: Lego Robotics",
+            "date": "23/7/2018",
+            "dow": "Mon",
+        }
     },
     {
-        "eventname": "3D Design for 3D Printing",
-        "date": "24/7/2018",
-        "dow": "Tue",
-        "starttime": "11:00",
-        "endtime": "13:00",
-        "cost": "8",
-        "plu": "",
-        "description": u''.join(
-            "Learn how to create a 3D object ready for printing on our "
-            "3D printer. "
-            u"(Printing is optional, £2 cost of print based on print "
-            "duration and materials used. "
-            "Printing price NOT included in session price."
-        ),
-        "agerange": "8-13",
-        "numslots": 8,
+        "name": "20180724_3ddesign", 
+        "bases": ["summer_tuesday", "3D Design"],
+        "overrides": {
+            "eventname": "Summer Session: 3D Design for 3D Printing",
+            "date": "24/7/2018",
+            "dow": "Tue",
+        }
     },
+    {
+        "name": "20180725_summer_scratch", 
+        "bases": ["summer_weekday", "scratch"],
+        "overrides": {
+            "eventname": "Summer Session: Raspberry Pi Programming (Scratch)",
+            "date": "25/7/2018",
+            "dow": "Wed",
+        }
+    },
+    {
+        "name": "20180728_summer_python",
+        "bases": ["summer_weekend", "python"],
+        "overrides": {
+            "eventname": "Summer Session: Raspberry Pi Programming (Python)",
+            "date": "28/7/2018",
+        }
+    },
+
 ]
 
 
 if __name__ == '__main__':
     for event in the_events:
-        sheet = SignupSheet()
-        sheet.eventname = event["eventname"]
-        sheet.date = event["date"]
-        sheet.dow = event["dow"]
-        sheet.starttime = event["starttime"]
-        sheet.endtime = event["endtime"]
-        sheet.cost = event["cost"]
-        sheet.plu = event["plu"]
-        sheet.description = event["description"]
-        sheet.agerange = event["agerange"]
-        sheet.numslots = event["numslots"]
+        params = Event()
+        for base in event["bases"]:
+            params.override(params=event_types[base].params)
+        params.override(params=event["overrides"])
+
+        sheet = SignupSheet(params=params)
+        print sheet.params.params
         sheet.generate_pdf()
--- Makery_signup_sheets/gen_signup_sheet.py
+++ Makery_signup_sheets/gen_signup_sheet.py
@@ -4,107 +4,214 @@
 import subprocess
 import datetime
 import codecs
+import copy
 
 from pylatexenc.latexencode import utf8tolatex
 
 
-class SignupSheet(object):
-    def __init__(self):
-        self._params = {
-            "eventname": None,
-            "date": None,
-            "dow": None,
-            "starttime": None,
-            "endtime": None,
-            "cost": None,
-            "plu": None,
-            "description": None,
-            "agerange": None,
-            "numslots": None,
-        }
-        self._template = None
-        self._signup_sheet = None
-        self._tex_filename = None
+class Event(object):
+    def __init__(self, params=None, orig=None):
+        if orig is not None:
+           self.__dict__ = copy.deepcopy(orig.__dict__)
+        else:
+            self._cost = 8
+            self._eventname = None
+            self._date = None
+            self._dow = None
+            self._starttime = None
+            self._endtime = None
+            self._cost = None
+            self._plu = None
+            self._description = None
+            self._agerange = None
+            self._numslots = None
+        if params is not None:
+            self.override(params)
+
+    @property
+    def params(self):
+        params = {}
+        for key in [
+            "cost", "eventname", "date", "dow", "starttime", "endtime", "cost",
+            "plu", "description", "agerange", "numslots"
+        ]:
+            if getattr(self, "_" + key, None) is not None:
+                params[key] = getattr(self, "_" + key)
+        return params
 
     @property
     def eventname(self):
-        return self._params["eventname"]
+        return self._eventname
 
     @eventname.setter
     def eventname(self, value):
-        self._params["eventname"] = value
+        self._eventname = value
 
     @property
     def date(self):
-        return self._params["date"]
+        return self._date
 
     @date.setter
     def date(self, value):
-        self._params["date"] = value
+        self._date = value
 
     @property
     def dow(self):
-        return self._params["dow"]
+        return self._dow
 
     @dow.setter
     def dow(self, value):
-        self._params["dow"] = value
+        self._dow = value
 
     @property
     def starttime(self):
-        return self._params["starttime"]
+        return self._starttime
 
     @starttime.setter
     def starttime(self, value):
-        self._params["starttime"] = value
+        self._starttime = value
 
     @property
     def endtime(self):
-        return self._params["endtime"]
+        return self._endtime
 
     @endtime.setter
     def endtime(self, value):
-        self._params["endtime"] = value
+        self._endtime = value
 
     @property
     def cost(self):
-        return self._params["cost"]
+        return self._cost
 
     @cost.setter
     def cost(self, value):
-        self._params["cost"] = value
+        self._cost = value
 
     @property
     def plu(self):
-        return self._params["plu"]
+        return self._plu
 
     @plu.setter
     def plu(self, value):
-        self._params["plu"] = value
+        self._plu = value
 
     @property
     def description(self):
-        return self._params["description"]
+        return self._description
 
     @description.setter
     def description(self, value):
-        self._params["description"] = value
+        self._description = value
 
     @property
     def agerange(self):
-        return self._params["agerange"]
+        return self._agerange
 
     @agerange.setter
     def agerange(self, value):
-        self._params["agerange"] = value
+        self._agerange = value
 
     @property
     def numslots(self):
-        return self._params["numslots"]
+        return self._numslots
 
     @numslots.setter
     def numslots(self, value):
-        self._params["numslots"] = value
+        self._numslots = value
+
+    def override(self, params):
+        for key, val in params.iteritems():
+            setattr(self, "_" + key, val)
+
+
+class SignupSheet(object):
+    def __init__(self, params=None):
+        self._params = None
+        self._template = None
+        self._signup_sheet = None
+        self._tex_filename = None
+        if params is not None:
+            self._params = params
+
+    @property
+    def eventname(self):
+        return self.params.eventname
+
+    @eventname.setter
+    def eventname(self, value):
+        self.params.eventname = value
+
+    @property
+    def date(self):
+        return self.params.date
+
+    @date.setter
+    def date(self, value):
+        self.params.date = value
+
+    @property
+    def dow(self):
+        return self.params.dow
+
+    @dow.setter
+    def dow(self, value):
+        self.params.dow = value
+
+    @property
+    def starttime(self):
+        return self.params.starttime
+
+    @starttime.setter
+    def starttime(self, value):
+        self.params.starttime = value
+
+    @property
+    def endtime(self):
+        return self.params.endtime
+
+    @endtime.setter
+    def endtime(self, value):
+        self.params.endtime = value
+
+    @property
+    def cost(self):
+        return self.params.cost
+
+    @cost.setter
+    def cost(self, value):
+        self.params.cost = value
+
+    @property
+    def plu(self):
+        return self.params.plu
+
+    @plu.setter
+    def plu(self, value):
+        self.params.plu = value
+
+    @property
+    def description(self):
+        return self.params.description
+
+    @description.setter
+    def description(self, value):
+        self.params.description = value
+
+    @property
+    def agerange(self):
+        return self.params.agerange
+
+    @agerange.setter
+    def agerange(self, value):
+        self.params.agerange = value
+
+    @property
+    def numslots(self):
+        return self.params.numslots
+
+    @numslots.setter
+    def numslots(self, value):
+        self.params.numslots = value
 
     @property
     def template(self):
@@ -117,6 +224,10 @@ class SignupSheet(object):
         if self._params is None:
             self.set_test_params()
         return self._params
+
+    @params.setter
+    def params(self, new_params):
+        self._params = new_params
 
     @property
     def signup_sheet(self):
@@ -146,7 +257,7 @@ class SignupSheet(object):
             self._template = template_file.read()
 
     def set_test_params(self):
-        self._params = {
+        self._params = Event(params={
             "eventname": "Raspberry Pi (Python)",
             "date": "2/7/2018",
             "dow": "Mon",
@@ -160,11 +271,11 @@ class SignupSheet(object):
             ),
             "agerange": "8-13",
             "numslots": 8,
-        }
+        })
 
     def generate_signupsheet(self):
         latex_params = {}
-        for name, param in self.params.iteritems():
+        for name, param in self.params.params.iteritems():
             latex_params[name] = utf8tolatex(param)
 
         self.signup_sheet = self.template.format(**latex_params)
@@ -186,4 +297,5 @@ class SignupSheet(object):
 
 if __name__ == '__main__':
     the_signupsheet = SignupSheet()
+
     the_signupsheet.generate_pdf()
